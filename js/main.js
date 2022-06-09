9f8f0053d4449bccc953aa4f903e5436fc3a0c87
Jordan Veloso <jrveloso96@gmail.com>
2022-06-08 23:22:21 -0500

    Added outcomes

--- js/main.js
+++ js/main.js
@@ -17,4 +17,38 @@ async function choice(){
         botChoice = 'scissors'
     }
     result.textContent = botChoice
+
+    // if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+    //     ++playerScore;
+    //     let win = 'Ok ok, nice one. Rock smashed Scissors';
+    //     return win; 
+    // } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+    //     ++playerScore;
+    //     let win = 'Good for you! Paper covered Rock';
+    //     return win; 
+    // } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+    //     ++playerScore;
+    //     let win = 'Solid guess. Scissors cut Paper';
+    //     return win; 
+    // } else if ((playerSelection === 'rock' && computerSelection === 'Rock') || 
+    // (playerSelection === 'paper' && computerSelection === 'Paper') || 
+    // (playerSelection === 'scissors' && computerSelection === 'Scissors')) {
+    //     let tie = 'Draw';
+    //     return tie; 
+    // } else if (playerSelection === 'rock' && computerSelection === 'Paper') {
+    //     ++computerScore;
+    //     let lose = 'Paper covered Rock. Loser!';
+    //     return lose; 
+    // } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+    //     ++computerScore;
+    //     let lose = 'Scissors cut Paper. You are garbage!';
+    //     return lose; 
+    // } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+    //     ++computerScore;
+    //     let lose = 'Rock broke Scissors. Yikes!';
+    //     return lose; 
+    // } else {
+    //     let error = 'Please click Rock, Paper or Scissors';
+    //     return error; 
+    // }
 }
